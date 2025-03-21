ad5d59184d1e3cc348c12b8e65888e56946c96e9
Juan Vicente Herrera <juaherre@redhat.com>
2025-03-21 18:08:24 +0100

    removed storage function

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-from google.cloud import storage
 
-def process_file(event, context):
-    """Triggered when a file is uploaded to Cloud Storage."""
-    file_name = event['name']
-    bucket_name = event['bucket']
-    
-    print(f"File {file_name} uploaded to {bucket_name}. Processing...")
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(file_name)
-
-    # Leer contenido (solo si es un archivo de texto)
-    if file_name.endswith('.txt'):
-        content = blob.download_as_text()
-        print(f"File content: {content}")
-    
-    return f"Processed {file_name} successfully!"
 from flask import Request
 
 def http_hello_world(request: Request):
